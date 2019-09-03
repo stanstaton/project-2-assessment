6f347de9630a5bf8b852238e749d2d6219d46d3f
stanstaton <thatkidenzo@gmail.com>
2019-09-03 14:52:30 -0700

    finished assessment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models')
 
 const app = express();
 
@@ -9,7 +10,41 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', (req,res) => {
+    db.widget.findAll()
+    .then(widgets => {
+        res.render('index', {widgets});
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('something went wrong')
+    })
+    
+})
 
+app.post('/', (req,res) => {
+    db.widget.create(req.body)
+    .then(() =>{
+		res.redirect('/')
+	})
+	.catch(err => {
+		console.log('something went wrong')
+		res.send('something went wrong')
+	})
+})
+
+app.delete('/', (req,res) => {
+    db.widget.destroy({
+        where: {id: req.body.id}
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('something went wrong')
+    })
+})
 
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
